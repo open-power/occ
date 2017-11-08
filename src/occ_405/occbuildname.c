72bb381838662b0656224e11bd56af2980c54880
William Bryan <wilbryan@us.ibm.com>
2017-11-08 17:20:25 -0600

    Update buildname 11/8
    Change-Id: I552814a707de5fc10f064082899caa12182812a6
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/49456
    Reviewed-by: William A. Bryan <wilbryan@us.ibm.com>
    Tested-by: William A. Bryan <wilbryan@us.ibm.com>

--- src/occ_405/occbuildname.c
+++ src/occ_405/occbuildname.c
@@ -34,6 +34,6 @@ volatile const char G_occ_buildname[16] __attribute__((section(".buildname"))) =
 
 #else
 
-volatile const char G_occ_buildname[16] __attribute__((section(".buildname"))) = /*<BuildName>*/  "op910_171103a\0"  /*</BuildName>*/ ;
+volatile const char G_occ_buildname[16] __attribute__((section(".buildname"))) = /*<BuildName>*/  "op910_171108a\0"  /*</BuildName>*/ ;
 
 #endif
