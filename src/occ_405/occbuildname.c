027159cc807175ea347295f4687cec33fd97c55e
Chris Cain <cjcain@us.ibm.com>
2017-11-29 10:52:12 -0600

    Initialize frequency votes to prevent throttling when booted in oversubscription
    Change-Id: Ia7b790e224909d7f6bbe4f827d7a162682d9f0a6
    CQ: SW409666
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/50144
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Martha Broyles <mbroyles@us.ibm.com>
    Reviewed-by: Andres A. Lugo-Reyes <aalugore@us.ibm.com>
    Reviewed-by: William A. Bryan <wilbryan@us.ibm.com>
    Reviewed-by: Christopher J. Cain <cjcain@us.ibm.com>

--- src/occ_405/occbuildname.c
+++ src/occ_405/occbuildname.c
@@ -34,6 +34,6 @@ volatile const char G_occ_buildname[16] __attribute__((section(".buildname"))) =
 
 #else
 
-volatile const char G_occ_buildname[16] __attribute__((section(".buildname"))) = /*<BuildName>*/  "op910_171121a\0"  /*</BuildName>*/ ;
+volatile const char G_occ_buildname[16] __attribute__((section(".buildname"))) = /*<BuildName>*/  "op910_171129a\0"  /*</BuildName>*/ ;
 
 #endif
