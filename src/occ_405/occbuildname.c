00123c66a1d23b3a4fca98528989d4e35550fd3d
William Bryan <wilbryan@us.ibm.com>
2018-01-09 10:46:11 -0600

    Update GPE1 Binary 1/16/18
    Change-Id: I12d62b8b769fed9b6cdb2189676ac773f7714cf8
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/52013
    Reviewed-by: Martha Broyles <mbroyles@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: William A. Bryan <wilbryan@us.ibm.com>

--- src/occ_405/occbuildname.c
+++ src/occ_405/occbuildname.c
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER OnChipController Project                                     */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2014,2017                        */
+/* Contributors Listed Below - COPYRIGHT 2014,2018                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -34,6 +34,6 @@ volatile const char G_occ_buildname[16] __attribute__((section(".buildname"))) =
 
 #else
 
-volatile const char G_occ_buildname[16] __attribute__((section(".buildname"))) = /*<BuildName>*/  "op_occ_180115a\0"  /*</BuildName>*/ ;
+volatile const char G_occ_buildname[16] __attribute__((section(".buildname"))) = /*<BuildName>*/  "op_occ_180116a\0"  /*</BuildName>*/ ;
 
 #endif
