9047e572b197876b77fa518bfcec6ed3be4e7560
mbroyles <mbroyles@us.ibm.com>
2020-10-20 12:15:06 -0500

    Power cap mismatch error fix
    Change-Id: Iacc839cf1d5f8aae531b7c647ab3789c45499dd4
    CQ: SW509948
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/106420
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Christopher J Cain <cjcain@us.ibm.com>
    Reviewed-by: Martha Broyles <mbroyles@us.ibm.com>

--- src/occ_405/occbuildname.c
+++ src/occ_405/occbuildname.c
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER OnChipController Project                                     */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2014,2019                        */
+/* Contributors Listed Below - COPYRIGHT 2014,2020                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -34,6 +34,6 @@ volatile const char G_occ_buildname[16] __attribute__((section(".buildname"))) =
 
 #else
 
-volatile const char G_occ_buildname[16] __attribute__((section(".buildname"))) = /*<BuildName>*/  "op_940_200728a\0"  /*</BuildName>*/ ;
+volatile const char G_occ_buildname[16] __attribute__((section(".buildname"))) = /*<BuildName>*/  "op_940_201020a\0"  /*</BuildName>*/ ;
 
 #endif
